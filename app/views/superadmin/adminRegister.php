30d2ae0d416ba1b1fe871f5b02bfe6b9d8bc238a
Ashok94bsc <ashok.karunathilake@gmail.com>
2021-10-19 23:53:31 +0100

    Update adminRegister.php
    Co-Authored-By: tharindu830 <80748866+tharindu830@users.noreply.github.com>

--- app/views/superadmin/adminRegister.php
+++ app/views/superadmin/adminRegister.php
@@ -6,6 +6,10 @@
 <link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
 <!-- Main Style Css -->
 <link rel="stylesheet" href="<?= PROOT ?>css/pt_style.css" />
+
+<!-------------add title------------------->
+<title>Admin Registration</title>
+<!----------------------------------------->
 </head>
 
 <body class="form-v2">
